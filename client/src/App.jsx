e4a980e6aff3807c1405e0bc0b6104894a4b628d
ViosSixth <risimac@outlook.com>
2025-07-17 23:02:18 +0200

    additions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,29 @@ import { useState } from 'react'
 import './App.css'
 import { render, screen } from '@testing-library/react'
 
-
 import ErrorBoundary from './components/ErrorBoundary';
 import BugList from './components/BugList';
 
-axios.get(`${import.meta.env.VITE_API_URL}/bugs`)
 
 function App() {
+  const [bugs, setBugs] = useState([])
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_API_URL}/bugs`)
+      .then(res => setBugs(res.data))
+      .catch(err => setError(err.message))
+  }, [])
+
   return (
     <div className="container">
       <h1>Bug Tracker</h1>
       <ErrorBoundary>
-        <BugList />
+        {error ? <p>Error: {error}</p> : <BugList bugs={bugs} />}
       </ErrorBoundary>
     </div>
-  );
+  )
 }
+
 
 export default App;
